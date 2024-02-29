6b4ef58fbfc9e15c75da84d7316dd8086ebe5802
Douglas Eduardo <douglas.patusco@gmail.com>
2024-02-29 17:04:15 -0300

    fix api

--- src/Pages/Restaurant/index.tsx
+++ src/Pages/Restaurant/index.tsx
@@ -1,11 +1,11 @@
 import { useParams } from 'react-router-dom'
 
-import { useGetRestaurantQuery } from '../../services/api'
-
 import DishesList from '../../components/DishesList'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import Cover from '../../components/Cover'
+
+import { useGetRestaurantQuery } from '../../services/api'
 
 const Restaurant = () => {
   const { id } = useParams()
